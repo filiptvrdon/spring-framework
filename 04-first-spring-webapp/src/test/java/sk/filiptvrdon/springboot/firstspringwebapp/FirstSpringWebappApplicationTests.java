d3897ce77e0fcb4a145975dddaf5ba942e299cfe
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-07-20 13:12:30 +0200

    renaming

---
+++ 04-first-spring-webapp/src/test/java/sk/filiptvrdon/springboot/firstspringwebapp/FirstSpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.filiptvrdon.springboot.firstspringwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
